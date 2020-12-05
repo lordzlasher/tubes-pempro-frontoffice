f1537436dfccf0036f6eef3fb6b94c2f97ba3a85
(2005551063)I Putu Gede Suryadharma Adhi Natha <75299436+suryadarmaa@users.noreply.github.com>
2020-12-06 01:36:55 +0800

    Add files via upload

---
+++ Front Office/Login.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnLogin = New System.Windows.Forms.Button()
+        Me.Username = New System.Windows.Forms.Label()
+        Me.TxtUsername = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TxtPassword = New System.Windows.Forms.TextBox()
+        Me.BtnCancel = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BtnLogin
+        '
+        Me.BtnLogin.Location = New System.Drawing.Point(214, 258)
+        Me.BtnLogin.Name = "BtnLogin"
+        Me.BtnLogin.Size = New System.Drawing.Size(83, 45)
+        Me.BtnLogin.TabIndex = 0
+        Me.BtnLogin.Text = "Login"
+        Me.BtnLogin.UseVisualStyleBackColor = True
+        '
+        'Username
+        '
+        Me.Username.AutoSize = True
+        Me.Username.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Username.Location = New System.Drawing.Point(92, 167)
+        Me.Username.Name = "Username"
+        Me.Username.Size = New System.Drawing.Size(97, 24)
+        Me.Username.TabIndex = 1
+        Me.Username.Text = "Username"
+        '
+        'TxtUsername
+        '
+        Me.TxtUsername.Location = New System.Drawing.Point(214, 171)
+        Me.TxtUsername.Name = "TxtUsername"
+        Me.TxtUsername.Size = New System.Drawing.Size(241, 22)
+        Me.TxtUsername.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(97, 210)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 24)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Password"
+        '
+        'TxtPassword
+        '
+        Me.TxtPassword.Location = New System.Drawing.Point(214, 212)
+        Me.TxtPassword.Name = "TxtPassword"
+        Me.TxtPassword.Size = New System.Drawing.Size(241, 22)
+        Me.TxtPassword.TabIndex = 4
+        '
+        'BtnCancel
+        '
+        Me.BtnCancel.Location = New System.Drawing.Point(371, 258)
+        Me.BtnCancel.Name = "BtnCancel"
+        Me.BtnCancel.Size = New System.Drawing.Size(84, 45)
+        Me.BtnCancel.TabIndex = 5
+        Me.BtnCancel.Text = "Cancel"
+        Me.BtnCancel.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.PictureBox1.Image = Global.Front_Office.My.Resources.Resources.download
+        Me.PictureBox1.Location = New System.Drawing.Point(169, 8)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(300, 145)
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(578, 327)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.BtnCancel)
+        Me.Controls.Add(Me.TxtPassword)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TxtUsername)
+        Me.Controls.Add(Me.Username)
+        Me.Controls.Add(Me.BtnLogin)
+        Me.Name = "Login"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Login"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BtnLogin As System.Windows.Forms.Button
+    Friend WithEvents Username As System.Windows.Forms.Label
+    Friend WithEvents TxtUsername As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TxtPassword As System.Windows.Forms.TextBox
+    Friend WithEvents BtnCancel As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+
+End Class
