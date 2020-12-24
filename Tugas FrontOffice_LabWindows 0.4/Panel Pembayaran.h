847e7a3c64380c7ddcdd9bb7a2d852e40d378cf7
(2005551063)I Putu Gede Suryadharma Adhi Natha <75299436+suryadarmaa@users.noreply.github.com>
2020-12-24 22:55:42 +0800

    Add files via upload

---
+++ Tugas FrontOffice_LabWindows 0.4/Panel Pembayaran.h
@@ -0,0 +1,37 @@
+/**************************************************************************/
+/* LabWindows/CVI User Interface Resource (UIR) Include File              */
+/*                                                                        */
+/* WARNING: Do not add to, delete from, or otherwise modify the contents  */
+/*          of this include file.                                         */
+/**************************************************************************/
+
+#include <userint.h>
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+     /* Panels and Controls: */
+
+#define  PANEL_5                          1       /* callback function: PanelPb */
+#define  PANEL_5_TABLE_1                  2       /* control type: table, callback function: (none) */
+
+
+     /* Control Arrays: */
+
+          /* (no control arrays in the resource file) */
+
+
+     /* Menu Bars, Menus, and Menu Items: */
+
+          /* (no menu bars in the resource file) */
+
+
+     /* Callback Prototypes: */
+
+int  CVICALLBACK PanelPb(int panel, int event, void *callbackData, int eventData1, int eventData2);
+
+
+#ifdef __cplusplus
+    }
+#endif
